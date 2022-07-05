e0961e5eb76568a2dfcc66f22dd0a1a4c3a53046
Shylashree V <shylashree74@gmail.com>
2022-07-05 12:29:48 -0700

    commit

--- birthremainder/src/List.js
+++ birthremainder/src/List.js
@@ -4,12 +4,13 @@ const List = ({people}) => {
   return (
     <>
       {people.map((person) =>{
-        const{ id, name, age, image } = person;
+        const{ id, name, dob, age, image } = person;
         return (
             <artcile key={id} className='person'>
             <img src={image} alt={name}/>
             <div>
                 <h4>{name}</h4>
+                <p>{dob} </p>
                 <p>{age} years</p>
             </div>
         </artcile>
